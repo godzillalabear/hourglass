e879674241db9b2def15456920595551197ff316
Yushan Cheng <b10432020@gapps.ntust.edu.tw>
2020-06-25 23:09:24 +0800

    try listen device movement

--- index.js
+++ index.js
@@ -1,17 +1,45 @@
-if(window.DeviceOrientationEvent) {
-    window.addEventListener('deviceorientation', function(event) {
-  var a = document.getElementById('alpha',
-          b = document.getElementById('beta'),
-          g = document.getElementById('gamma'),
-          alpha = event.alpha,
-          beta = event.beta,
-              gamma = event.gamma;
+if (window.DeviceOrientationEvent) {
+    window.addEventListener("deviceorientation", function(event) {
+        // alpha: rotation around z-axis
+        var rotateDegrees = event.alpha;
+        
+        // gamma: left to right
+        var leftToRight = event.gamma;
+        
+        // beta: front back motion
+        var frontToBack = event.beta;
 
-  a.innerHTML = Math.round(alpha);
-  b.innerHTML = Math.round(beta);
-  g.innerHTML = Math.round(gamma);
 
-    }, false);
-}else{
-    document.querySelector('body').innerHTML = '你的瀏覽器不支援喔';
+        handleOrientationEvent(frontToBack, leftToRight, rotateDegrees);
+        console.log("yes");
+        document.body.innerHTML = "yes";
+    }, true);
+    
+    
+}else {
+    console.log("no");
+    document.body.innerHTML = "nope";
 }
+
+var handleOrientationEvent = function(frontToBack, leftToRight, rotateDegrees) {
+    // do something amazing
+    console.log("orientation");
+    console.log(rotateDegrees);
+    console.log(leftToRight);
+    console.log(frontToBack);
+};
+
+function handleMotionEvent(event) {
+
+    var x = event.accelerationIncludingGravity.x;
+    var y = event.accelerationIncludingGravity.y;
+    var z = event.accelerationIncludingGravity.z;
+
+    // Do something awesome.
+    console.log("xyz");
+    console.log(x);
+    console.log(y);
+    console.log(z);
+}
+
+window.addEventListener("devicemotion", handleMotionEvent, true);
